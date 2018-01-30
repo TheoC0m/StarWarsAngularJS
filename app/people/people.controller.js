1c0b98ce723ca19d275b57bb62f7ab8b91581dda
TheoC0m <theo.c.developer@gmail.com>
2018-01-30 02:47:38 +0100

    code optimization and cleaning on people files

--- app/people/people.controller.js
+++ app/people/people.controller.js
@@ -1,93 +1,84 @@
 angular.module('StarWarsAngularJS')
-    .controller('PeopleController', ['$scope', '$location', '$routeParams', 'PeopleService', 'ToolsFactory',
-        function ($scope, $location, $routeParams, PeopleService, ToolsFactory) {
+	.controller('PeopleController', ['$scope', '$location', '$routeParams', 'PeopleService', 'ToolsFactory',
+		function($scope, $location, $routeParams, PeopleService, ToolsFactory) {
 
+			$scope.getAllPeople = function() {
+				PeopleService.getAllPeople($scope.pageNumber)
+					.then(function(response) {
+						if (response != undefined) {
+							$scope.peoples = response;
+							console.log($scope.peoples);
+						}
+						$scope.loaded = true;
+					})
+			}
 
-            $scope.getAllPeople = function () {
-                PeopleService.getAllPeople($scope.pageNumber)
-                    .then(function (response) {
-                        if (response != undefined) {
-                            $scope.peoples = response;
-                            console.log($scope.peoples);
-                        }
-                    })
-            }
+			$scope.getPeople = function() {
+				PeopleService.getPeople($routeParams.peopleId)
+					.then(function(response) {
+						if (response != undefined) {
+							$scope.people = response;
+							console.log($scope.people);
+						}
+						
+						$scope.filmsUrlName = ToolsFactory.getFilmUrlName(response.data.films);
+						$scope.vehiclesUrlName = ToolsFactory.getVehiclesUrlName(response.data.vehicles);
+						$scope.starshipsUrlName = ToolsFactory.getStarshipsUrlName(response.data.starships);
+						$scope.planetsUrlName = ToolsFactory.getPlanetsUrlName(response.data.homeworld);
+						$scope.speciesUrlName = ToolsFactory.getSpeciesUrlName(response.data.species);
 
-            $scope.getPeople = function () {
-                PeopleService.getPeople($routeParams.peopleId)
-                    .then(function (response) {
-                        $scope.vehiclesUrlName = [];
-                        $scope.vehiclesUrlName = ToolsFactory.getVehiclesUrlName(response.data.vehicles);
+						$scope.loaded = true;
+					})
+			}
 
-                        $scope.filmsUrlName = [];
-                        $scope.filmsUrlName = ToolsFactory.getFilmUrlName(response.data.films);
+			$scope.searchPeople = function() {
+				PeopleService.searchPeople($scope.peopleSearched)
+					.then(function(response) {
+						if (response != undefined) {
+							console.log(response);
+							console.log($scope.peopleSearched);
+							$scope.searchResult = response;
+						}
+					})
+			}
 
+			$scope.extractUrlId = function(string) {
+				return ToolsFactory.extractUrlId(string);
+			}
 
-                        $scope.planetsUrlName = [];
-                        $scope.planetsUrlName = ToolsFactory.getPlanetsUrlName(response.data.homeworld);
+			$scope.pageNumberIncrement = function() {
+				$scope.pageNumber++;
+				$scope.getAllPeople();
+			}
 
-                        $scope.starshipsUrlName = [];
-                        $scope.starshipsUrlName = ToolsFactory.getStarshipsUrlName(response.data.starships);
+			$scope.pageNumberDecrement = function() {
+				if ($scope.pageNumber > 1)
+					$scope.pageNumber--;
+				$scope.getAllPeople();
+			}
 
-                        $scope.speciesUrlName = [];
-                        $scope.speciesUrlName = ToolsFactory.getSpeciesUrlName(response.data.species);
+			$scope.start = function() {
+				console.log($routeParams);
+				$scope.loaded = false;
+				$scope.peopleSearched = "test";
+				$scope.searchResult = null;
+				$scope.people = null;
+				$scope.peoples = null;
 
-                        if (response != undefined) {
+				$scope.filmsUrlName = [];
+				$scope.vehiclesUrlName = [];
+				$scope.planetsUrlName = [];
+				$scope.starshipsUrlName = [];
+				$scope.speciesUrlName = [];
 
-                            $scope.people = response;
-                            console.log($scope.people);
+				if ($routeParams.peopleId) {
+					$scope.getPeople();
+				} else {
+					$scope.pageNumber = 1;
+					$scope.getAllPeople();
+				}
+			}
 
-                        }
-                    })
-            }
-
-            $scope.searchPeople = function () {
-                PeopleService.searchPeople($scope.peopleSearched)
-                    .then(function (response) {
-                        if (response != undefined) {
-                            console.log(response);
-                            console.log($scope.peopleSearched);
-                            $scope.searchResult = response;
-                        }
-                    })
-            }
-
-            $scope.extractUrlId = function (string) {
-                return ToolsFactory.extractUrlId(string);
-            }
-
-            $scope.pageNumberIncrement = function () {
-                $scope.pageNumber++;
-                $scope.getAllPeople();
-            }
-
-            $scope.pageNumberDecrement = function () {
-                if ($scope.pageNumber > 1)
-                    $scope.pageNumber--;
-                $scope.getAllPeople();
-            }
-
-            $scope.start = function () {
-                console.log($routeParams);
-                $scope.peopleSearched = "test";
-                $scope.searchResult = null;
-                $scope.people = null;
-                $scope.peoples = null;
-                $scope.filmNames = [];
-
-
-                if ($routeParams.peopleId) {
-
-                    $scope.getPeople();
-
-                } else {
-
-                    $scope.pageNumber = 1;
-                    $scope.getAllPeople();
-                }
-            }
-
-            $scope.start();
-
-        }
-    ])
+			$scope.start();
+		}
+	])
